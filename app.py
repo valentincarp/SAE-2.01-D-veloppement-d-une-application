5a0045c1efff5174b88046a8d4a886dbc74d7962
Arthur_Sivault <arthur.sivault-le-morellec@etu.u-pec.fr>
2026-06-04 11:03:58 +0200

    ajout du fichier app.py qui exécute l'app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from config import Config
+from controllers.accueil import bp_accueil
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Enregistrement des contrôleurs (blueprints)
+app.register_blueprint(bp_accueil)
+
+if __name__ == "__main__":
+    app.run(debug=True)
