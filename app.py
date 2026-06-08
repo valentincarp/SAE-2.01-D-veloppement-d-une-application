39f7f7e9323d3e7fc900a07a9e9acdcb4f1f1e97
Arthur_Sivault <arthur.sivault-le-morellec@etu.u-pec.fr>
2026-06-08 10:58:35 +0200

    page d'erreur (404 et 500)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from controllers.accueil import bp_accueil
 from controllers.api import bp_api
@@ -11,6 +11,16 @@ app.config.from_object(Config)
 app.register_blueprint(bp_accueil)
 app.register_blueprint(bp_api)
 app.register_blueprint(bp_effectifs)
+
+@app.errorhandler(404)
+def page_non_trouvee(e):
+    return render_template("erreur.html",
+        message="Page non trouvée."), 404
+
+@app.errorhandler(500)
+def erreur_serveur(e):
+    return render_template("erreur.html",
+        message="Erreur interne. Réessayez plus tard."), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
