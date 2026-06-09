a0d0a31da0f65a7b3ccf6aa2916593498f562265
m_nohlan <nohlan.momperousse@etu.u-pec.fr>
2026-06-09 12:37:55 +0200

    finalisation du fichier prescriptions.py

---
+++ controllers/Prescription.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, render_template, request
+from models.db import Session
+from models.dimensions import ProfessionSante, Departement
+from services.ameli_api import AmeliAPI
+
+bp_prescription = Blueprint("prescriptions", __name__)
+api = AmeliAPI()
+
+@bp_prescription.route("/prescriptions")
+def afficher():
+    """Affiche les effectifs pour la sélection de l'utilisateur."""
+    profession_id = request.args.get("profession_id", type=int)
+    departement_id = request.args.get("departement_id", type=int)
+    annee = request.args.get("annee", type=int)
+    poste_prescription = request.args.get("poste_prescription", type=int)
+
+    session = Session()
+    try:
+        prof = session.get(ProfessionSante, profession_id)
+        dept = session.get(Departement, departement_id)
+        if not prof or not dept or not annee:
+            return render_template("erreur.html", message="Paramètres manquants."), 400
+        resultats = api.get_prescriptions(prof.libelle, dept.code, annee, poste_prescription)
+        evolution = api.get_evolution_prescriptions(prof.libelle, dept.code)
+
+        return render_template("prescriptions.html",
+            prof=prof, dept=dept, annee=annee,
+            resultats=resultats, evolution=evolution)
+    finally:
+        session.close()
