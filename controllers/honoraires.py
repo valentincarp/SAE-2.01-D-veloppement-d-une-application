609f251ae3b235a8bb04cdcfef8144c70e2fb657
Valentin Carpinteiro <valentincarpinteiro@vaal.local>
2026-06-10 14:01:29 +0200

    ajout de commentaires pour honoraires.py

--- controllers/honoraires.py
+++ controllers/honoraires.py
@@ -9,7 +9,7 @@ api = AmeliAPI()
 @bp_honoraires.route("/honoraires")
 def afficher():
     """Affiche les honoraires selon les filtres sélectionnés."""
-    # 1. On récupère les paramètres de l'URL (qui valent None au premier affichage)
+    #on récupère les paramètres de l'URL
     profession_id = request.args.get("profession_id", type=int)
     departement_id = request.args.get("departement_id", type=int)
     type_honoraire_id = request.args.get("type_honoraire_id", type=int)
@@ -17,30 +17,26 @@ def afficher():
 
     session = Session()
     try:
-        # 2. On récupère TOUJOURS les données pour les <select> du formulaire HTML
+        #on récupère les données pour les select du formulaire HTML
         toutes_professions = session.query(ProfessionSante).all()
         tous_departements = session.query(Departement).all()
         tous_types_honoraires = session.query(TypeHonoraire).all()
         
-        # Variables pour stocker la recherche de l'utilisateur (vides par défaut)
+        #pour stocker la recherche de l'utilisateur (vides par défaut) 
         prof_selectionne = None
         dept_selectionne = None
         hono_selectionne = None
         donnees_api = None
 
-        # 3. SI le formulaire a été soumis (tous les paramètres sont là)
+        #if le formulaire a été soumis alors on recupere les infos
         if profession_id and departement_id and type_honoraire_id and annee:
             prof_selectionne = session.get(ProfessionSante, profession_id)
             dept_selectionne = session.get(Departement, departement_id)
             hono_selectionne = session.get(TypeHonoraire, type_honoraire_id)
-            
             if prof_selectionne and dept_selectionne and hono_selectionne:
-                # C'est ici qu'on va appeler l'API !
-                # Exemple : donnees_api = api.get_honoraires(prof_selectionne.libelle, dept_selectionne.code, annee, hono_selectionne.niveau_1)
-                # Dans ton if :
                 donnees_api = api.get_honoraires(prof_selectionne.libelle, dept_selectionne.code, annee)                
         
-        # 4. On renvoie le template unique de ta page
+        #on renvoie le template de la page
         return render_template("honoraires.html",
                                professions=toutes_professions,
                                departements=tous_departements,
