1938e2fad4b609d5cf53512265974a5d2b5590e8
m_nohlan <nohlan.momperousse@etu.u-pec.fr>
2026-06-10 10:43:57 +0200

    creation du fichiers prescriptions.html

--- controllers/Prescription.py
+++ controllers/Prescription.py
@@ -20,7 +20,7 @@ def afficher():
         if not prof or not dept or not annee:
             return render_template("erreur.html", message="Paramètres manquants."), 400
         resultats = api.get_prescriptions(prof.libelle, dept.code, annee, poste_prescription)
-        evolution = api.get_evolution_prescriptions(prof.libelle, dept.code)
+        evolution = api.get_evolution_prescriptions(prof.libelle, dept.code, poste_prescription)
 
         return render_template("prescriptions.html",
             prof=prof, dept=dept, annee=annee,
