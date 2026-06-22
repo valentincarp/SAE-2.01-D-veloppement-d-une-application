c875bab581ea45c3d13c49133b6d615d2d095e2e
Arthur_Sivault <arthur.sivault-le-morellec@etu.u-pec.fr>
2026-06-22 11:20:55 +0200

    refactoring et ajout css

--- controllers/admin.py
+++ controllers/admin.py
@@ -8,13 +8,13 @@ bp_admin = Blueprint("admin", __name__, url_prefix="/admin")
 @bp_admin.route("/")
 @login_required
 def index():
-    db_session = Session()
+    session = Session()
     try:
         # On affiche des statistiques rapides sur le panneau d'administration
-        nb_professions = db_session.query(ProfessionSante).count()
-        nb_regions = db_session.query(Region).count()
-        nb_departements = db_session.query(Departement).count()
-        nb_prescriptions = db_session.query(TypePrescription).count()
+        nb_professions = session.query(ProfessionSante).count()
+        nb_regions = session.query(Region).count()
+        nb_departements = session.query(Departement).count()
+        nb_prescriptions = session.query(TypePrescription).count()
         
         return render_template("admin.html", 
                                nb_professions=nb_professions,
@@ -22,4 +22,4 @@ def index():
                                nb_departements=nb_departements,
                                nb_prescriptions=nb_prescriptions)
     finally:
-        db_session.close()
+        session.close()
